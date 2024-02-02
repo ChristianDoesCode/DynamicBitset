882a4cec0c3aebf11e8694040b97bc87beb1837c
ChristianDoesCode <70384649+ChristianDoesCode@users.noreply.github.com>
2024-02-02 13:52:40 -0600

    Update DynamicBitset.h

--- DynamicBitset/DynamicBitset.h
+++ DynamicBitset/DynamicBitset.h
@@ -2,33 +2,36 @@
 #include <cstring>
 #include <iostream>
 
-class DynamicBitset
+namespace cpStdLib
 {
-private:
-	unsigned char* array;
-	unsigned int arrayLength;
-	unsigned int bitLength;
-	bool hasBeenCopied;
-
-public:
-	//Constructors
-	DynamicBitset();
-	DynamicBitset(unsigned int bitLength);
-	//Copy Constructor
-	DynamicBitset(DynamicBitset& originalBitset);
-	//Deconstructor
-	~DynamicBitset();
-
-	unsigned int getArrayLength();
-
-	//function to add a byte to the bitset
-	void addByte(unsigned char byte);
-	void zeroOutArray();
-	void zeroUpTo(unsigned int bitIndex);
-	void operator++(int);
-	//DynamicBitset operator++(int);
-
-	bool operator[](unsigned int index);
-
-	void printArray();
-};
+	class DynamicBitset
+	{
+	private:
+		unsigned char* array;
+		unsigned int arrayLength;
+		unsigned int bitLength;
+		bool hasBeenCopied;
+	
+	public:
+		//Constructors
+		DynamicBitset();
+		DynamicBitset(unsigned int bitLength);
+		//Copy Constructor
+		DynamicBitset(DynamicBitset& originalBitset);
+		//Deconstructor
+		~DynamicBitset();
+	
+		unsigned int getArrayLength();
+	
+		//function to add a byte to the bitset
+		void addByte(unsigned char byte);
+		void zeroOutArray();
+		void zeroUpTo(unsigned int bitIndex);
+		void operator++(int);
+		//DynamicBitset operator++(int);
+	
+		bool operator[](unsigned int index);
+	
+		void printArray();
+	};
+}
